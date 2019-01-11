cfa291440c605c345e84333ff606e0eabb49f722
Venkatesh <balvenkatesh@deloitte.com>
2019-01-11 17:03:27 +0530

    

---
+++ HelloPython.py
@@ -0,0 +1,18 @@
+class Employee:
+  def __init__(self,name,id,designation):
+    self.name=name
+    self.id=id
+    self.designation=designation
+
+  def print_values(self):
+    print(self.name + "\n" + self.id + "\n" + self.designation)
+
+name=input("Enter the name")
+id=input("Enter the ID")
+designation=input("enter the designation")
+emp=Employee(name,id,designation)
+emp.id="999"
+print(emp.id)
+emp.print_values()
+
+
